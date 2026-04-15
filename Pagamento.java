e9f5e2f314d2ecb36ac60c6bec02e88905798059
pedro.perroni <pedro.raphael.perroni@gmail.com>
2026-04-15 08:29:57 -0300

    Add files via upload

---
+++ Pagamento.java
@@ -0,0 +1,55 @@
+package com.exemplo.usuariosimples.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "pagamentos")
+public class Pagamento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Double valor;
+
+    @Column(nullable = false, length = 50)
+    private String status;
+
+    public Pagamento() {
+    }
+
+    public Pagamento(Double valor, String status) {
+        this.valor = valor;
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
